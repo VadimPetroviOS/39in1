32905f94d2f0bd7fca389f4d166c91bf7b0cca0d
Vadim <vadim@MacBook-Air-Kristina.local>
2025-02-19 12:14:08 +0300

    first commit

---
+++ 39in1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  39in1
+//
+//  Created by Vadim on 19.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
